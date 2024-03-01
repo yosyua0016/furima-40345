f6ceaee67aed6cb10745278b6ac7542692b9dc1b
yosyua0016 <159263548+yosyua0016@users.noreply.github.com>
2024-03-01 15:23:10 +0900

    テストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -66,6 +66,13 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include('Password Include both letters and numbers')
       end
 
+      it 'passwordが全角文字を含むパスワードでは登録できない' do
+        @user.password = 'あいうえおか11' 
+        @user.password_confirmation = 'あいうえおか11' 
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password Include both letters and numbers"
+      end
+
       it 'passwordとpassword_confirmationが不一致では登録できない' do
         @user.password = '123456'
         @user.password_confirmation = '1234567'
