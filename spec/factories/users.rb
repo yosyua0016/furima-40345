ac01ff87449800d2bd9f8a2db04695eb9d094770
yosyua0016 <159263548+yosyua0016@users.noreply.github.com>
2024-03-01 15:02:26 +0900

    確実に半角英数混合の値を生成するため修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.name }
     email { Faker::Internet.email }
-    password { Faker::Internet.password(min_length: 6) }
+    password { Faker::Internet.password(min_length: 6) + '11'}
     password_confirmation { password }
     first_name { '太郎' }
     last_name { '田中' }
