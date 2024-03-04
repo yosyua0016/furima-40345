83156ea26495d5ac1367c4d71c7feed3f57e6690
yosyua0016 <159263548+yosyua0016@users.noreply.github.com>
2024-03-04 13:59:14 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,19 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :image, :name, :description, :category_id, :condition_id, :ship_area_id, :ship_date_id, :ship_method_id,
-            :price, presence: true
+  validates :image,        presence: true
+
+  validates :name,         presence: true
+  validates :description,  presence: true
+  validates :category_id,  presence: true, numericality: { other_than: 0 }
+  validates :condition_id, presence: true, numericality: { other_than: 0 }
+  validates :ship_area_id, presence: true, numericality: { other_than: 0 }
+  validates :ship_date_id, presence: true, numericality: { other_than: 0 }
+  validates :ship_method_id, presence: true, numericality: { other_than: 0 }
+  validates :price,           presence: true,
+                              numericality: {
+                                only_integer: true,
+                                greater_than_or_equal_to: 300,
+                                less_than_or_equal_to: 9_999_999
+                              }
 end
