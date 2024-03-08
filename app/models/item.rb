c69526b51436ff15e19a0d62d4b3a6c1e63b5d3b
yosyua0016 <159263548+yosyua0016@users.noreply.github.com>
2024-03-08 21:49:32 +0900

    追加修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_one :order
+  has_one :order_form
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
   belongs_to_active_hash :ship_area
