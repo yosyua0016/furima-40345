7a9755119e6e8dcd51636bb94fb79619e8edc498
yosyua0016 <159263548+yosyua0016@users.noreply.github.com>
2024-03-10 22:56:14 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,7 +21,7 @@ class Item < ApplicationRecord
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }
   end
 
-  with_options numericality: { other_than: 0 } do
+  with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :condition_id
     validates :ship_area_id
