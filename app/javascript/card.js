1a6bc3fedfd3b8e17bbfbd1412f5a66f6f9493a2
yosyua0016 <159263548+yosyua0016@users.noreply.github.com>
2024-03-11 17:57:11 +0900

    Update card.js

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,7 @@
 const pay = () => {
-  console.log("カード情報トークン化のためのJavaScript");
-  const payjp = Payjp(publicKey)
+  const payjp = Payjp(publicKey)// PAY.JPテスト公開鍵
   const form = document.getElementById('charge-form')
-  form.addEventLcistener("submit", (e) => {
+  form.addEventListener("submit", (e) => {
     console.log("フォーム送信時にイベント発火")
     e.preventDefault();
   });
