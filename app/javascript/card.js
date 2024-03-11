b431b890c565afc3bc22edfbc2059b8b1d9302d5
yosyua0016 <159263548+yosyua0016@users.noreply.github.com>
2024-03-11 15:36:25 +0900

    Update card.js

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -36,5 +36,5 @@ const pay = () => {
     }
   };
 
-  window.addEventListener("turbo:load", pay)
-  window.addEventListener("turbo:render", pay)
+window.addEventListener("turbo:load", pay)
+window.addEventListener("turbo:render", pay)
