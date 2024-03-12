f6e508fa0e4cbd26f529f59b1776edd9e2d9cf1e
yosyua0016 <159263548+yosyua0016@users.noreply.github.com>
2024-03-12 10:35:21 +0900

    修正
    都道府県」が空欄だと不可

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -6,7 +6,7 @@ class OrderForm
     validates :item_id
     validates :user_id
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :ship_area_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :ship_area_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :street
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Exclude hyphen(-)' }
